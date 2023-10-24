4f91cbeaf71fdf51dd243c8fccfa70927b2dab80
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2023-10-24 20:25:07 -0300

    salvando ate aqui

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-let num = document.getElementById('num0').value
+let num = document.getElementById('num0')
 let res = document.getElementById('res')
 let elementosAdicionados = 0
 
 dias = []
+num.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        colocar();
+    }
+})
 
 function colocar(){
+    
     if (elementosAdicionados < 3){
     dias.push(Number(num.value))
     num.value = ''
     num.focus()
     elementosAdicionados++
-    let diasNaoUteis = (Number(dias[1]))-(Number(dias[2]))
-    let dsr = diasNaoUteis/(Number(dias[2]))
-    calculoFolha(Number(dias[0]))
-    let total = totalSemDSR*dsr
-    
-    res.innerHTML = `${total}`
+    res.innerHTML = `Lista: ${dias}`
     } else {
-        window.alert('Voce ja adicionou os 3 elementos')
+        window.alert('Voce ja adicionou três elementos')
+    }
+
+    if (dias.length === 3){
+        let diasTrabalhados = dias[0]
+        let diasTotais = dias[1]
+        let diasUteis = dias[2]
+        let diasNaoUteis = diasTotais - diasUteis
+
+        let dsrPorcentagem = diasNaoUteis/diasUteis
+        let horasTrabalhadas = diasTrabalhados*6
+        let salSemDsr = horasTrabalhadas*6
+        let dsr = salSemDsr*dsrPorcentagem
+
+        let salarioBruto = salSemDsr+dsr
+        
+        window.alert(salarioBruto)
     }
 }
 
-function calculoFolha(diasTrabalhados){
-    let horasTrabalhadas = diasTrabalhados*6
-    let totalSemDSR = horasTrabalhadas*6
-    return totalSemDSR
-}
+
 
